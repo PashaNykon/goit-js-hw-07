50c7efb1d650bb7316602d222f7ca8fbc334b468
PashaNykon <pavlonykon@ukr.net>
2023-05-30 20:13:57 +0300

    Розмітка для 1 завдання

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+function createGelleryItems(Items) {
+    return galleryItems
+        .map(item => {
+        return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+        <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+     />
+    </a>
+    </li>
+    `;
+    })
+        .join('');
+ }
+ 
+const gelleryItemsList = document.querySelector('.gallery');
+gelleryItemsList.insertAdjacentHTML('afterbegin', createGelleryItems(galleryItems)); 
+
+const onImageClick = document.querySelector('gallery__link');
+onImageClick.addEventListener('click', (event) => {
+    event.preventDefault();
+})
